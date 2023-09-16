369024a2b97d7ec11d0c545a0240dff2712241e9
Richa-12y <kricha000@gmail.com>
2023-09-16 22:18:43 +0530

    today code added

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import CustomerReview from "./CustomerReview";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import Footer from "./Footer";
+
+export {
+  Hero,
+  CustomerReview,
+  Services,
+  PopularProducts,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+  Footer,
+};
